1f47dac3ac151069bd58656176a27efd330e16ba
andresindlp <35582438+andresindlp@users.noreply.github.com>
2022-10-03 00:06:29 +0200

    added .quote funcionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const ffmpeg = require('fluent-ffmpeg');
 const qrcode = require('qrcode-terminal');
 const moment = require('moment-timezone');
 const colors = require('colors');
 const fs = require('fs');
+const PImage = require('pureimage');
+
 
 const client = new Client({
     restartOnAuthFail: true,
@@ -15,7 +17,9 @@ const client = new Client({
 });
 
 const config = require('./config/config.json');
-
+const template = "./files/template_msg.png";
+const filepath = "./files/output.png";
+const font = PImage.registerFont("./files/arial_bold.ttf","ArialBlack");
 
 client.on('qr', (qr) => {
     console.log(`[${moment().tz(config.timezone).format('HH:mm:ss')}] Scan the QR below : `);
@@ -41,7 +45,7 @@ client.on('message', async (message) => {
     if (message.body == ".s" && Chat.isGroup) {
         if (message.type == "image") {
             try {
-                Chat.sendStateTyping()
+                Chat.sendStateTyping();
                 const media = await message.downloadMedia();
                 client.sendMessage(message.from, media, {
                     sendMediaAsSticker: true,
@@ -57,7 +61,7 @@ client.on('message', async (message) => {
             }
         }
         else if (message.hasQuotedMsg) {
-            const quotedMsg = await message.getQuotedMessage()
+            const quotedMsg = await message.getQuotedMessage();
             if (quotedMsg.type == "image") {
                 try {
                     Chat.sendStateTyping()
@@ -96,10 +100,10 @@ client.on('message', async (message) => {
             message.react("❌");
         }
     } else if (message.body == ".reveal" && Chat.isGroup && message.hasQuotedMsg)  {
-        const quotedMsg = await message.getQuotedMessage()
+        const quotedMsg = await message.getQuotedMessage();
         if (quotedMsg.type == "image") {
             try {
-                Chat.sendStateTyping()
+                Chat.sendStateTyping();
                 const media = await quotedMsg.downloadMedia();
                 client.sendMessage(message.from, media).then(() => {
                     Chat.clearState();
@@ -111,8 +115,61 @@ client.on('message', async (message) => {
                 message.react("❌");
             }
         }
-    }
-    else {
+    } else if (message.body == ".quote" && Chat.isGroup && message.hasQuotedMsg) {
+        const quotedMsg = await message.getQuotedMessage();
+        if (true) {
+            /* try { */
+                Chat.sendStateTyping();
+                const authorID = await quotedMsg.getContact();
+                const msgAuthor = authorID.pushname;
+                const msgBody = quotedMsg.body;
+                const maxLength = 22;
+                let partOne = " ";
+                let partTwo = " ";
+                let partThree = " ";
+
+                if (msgBody.length >= 23) {
+                    partOne = msgBody.substring(0,maxLength);
+                    partTwo = msgBody.substring(maxLength, maxLength*2);
+                    
+                    if (msgBody.length >= 46) {
+                        partThree = msgBody.substring(maxLength*2, maxLength*3);
+                    }
+                    
+                }
+
+                font.load(() => {
+                    PImage.decodePNGFromStream(fs.createReadStream(template)).then((img) => {
+                        const ctx = img.getContext("2d");
+                        ctx.fillStyle = "#9a9af1";
+                        ctx.font = "36pt ArialBlack";
+                        ctx.fillText(msgAuthor, 50, 210);
+                        ctx.fillStyle = "#ffffff";
+                        ctx.fillText(partOne, 50, 260);
+                        ctx.fillText(partTwo, 50, 295);     
+                        ctx.fillText(partThree, 50, 330);
+                        PImage.encodePNGToStream(img, fs.createWriteStream(filepath)).then(()=>{
+                            const media = MessageMedia.fromFilePath(filepath);
+                            client.sendMessage(message.from, media, {
+                                sendMediaAsSticker: true,
+                                stickerName: config.name, // Sticker Name = Edit in 'config/config.json'
+                                stickerAuthor: `made with andrew's bot` // Sticker Author = Your Whatsapp BOT Number
+                            }).then(() => {
+                                Chat.clearState();
+                                quotedMsg.react("✅");
+                                message.react("✅");
+                            });
+                        });
+                    });
+                });
+            /* } catch {
+                Chat.clearState();
+                message.react("❌");
+            } */
+        } else {
+            message.react("❌");
+        }
+    } else {
         client.getChatById(message.id.remote).then(async (chat) => {
             await chat.sendSeen();
         });
